9dffecac46856cffef651506a5316a47043cf22a
sfelder <samfelder17@gmail.com>
2019-04-03 19:59:55 -0700

    finally completely done with oAuth

--- discuss/web/router.ex
+++ discuss/web/router.ex
@@ -7,6 +7,7 @@ defmodule Discuss.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug Discuss.Plugs.SetUser
   end
 
   pipeline :api do
@@ -29,6 +30,8 @@ defmodule Discuss.Router do
 
     get "/:provider", AuthController, :request
     get "/:provider/callback", AuthController, :callback
+    put "/signout", AuthController, :signout
+
   end
 
   # Other scopes may use custom stacks.
